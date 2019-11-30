1a21b72990e957e994049d3afc6d251825f8966a
unknown <surmaannalo@gmail.com>
2019-11-30 15:02:16 +0100

    dodano prostokat

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,30 +3,39 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-    double a = 0;
-    double b = 1;
-    double c = 2;
-    double e = 3;
-    double f = 4;
-    double g = 5;
+//    double a = 0;
+//    double b = 1;
+//    double c = 2;
+//    double e = 3;
+//    double f = 4;
+//    double g = 5;
     public static void wyswietl () {
-        String imie, nazwisko;
-        int wiek;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Wypisz imie");
-        imie = scanner.next();
-        System.out.println("Wypisz nazwisko");
-        nazwisko = scanner.next();
-        System.out.println("Wypisz wiek");
-        wiek = scanner.nextInt();
+//        String imie, nazwisko;
+//        int wiek;
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Wypisz imie");
+//        imie = scanner.next();
+//        System.out.println("Wypisz nazwisko");
+//        nazwisko = scanner.next();
+//        System.out.println("Wypisz wiek");
+//        wiek = scanner.nextInt();
 
 
+    }
+    public static void figura(){
+        for(int j=0;j<10;j++) {
+
+            for (int i = 0; i < 10; i++)
+                System.out.print("*");
+            System.out.println("");
+        }
     }
 
 
 
     public static void main(String[] args) {
-        wyswietl();
+
+        figura();
 
 
 
