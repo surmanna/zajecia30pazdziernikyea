6ef969a9327a6b4afab0ede85fc7be1b79869393
unknown <surmaannalo@gmail.com>
2019-11-30 13:12:13 +0100

    dodano definicje

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,14 @@
 package com.company;
 
 public class Main {
+    double a = 0;
+    double b = 1;
+    double c = 2;
+
 
     public static void main(String[] args) {
         System.out.println("napis");
+
 
 
 
