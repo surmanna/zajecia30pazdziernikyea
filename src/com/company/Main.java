4f5f7f9c909a310236edfd778a9d414b2598e695
unknown <surmaannalo@gmail.com>
2019-11-30 14:45:06 +0100

    dodano wizytowke

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
     double a = 0;
     double b = 1;
@@ -7,11 +9,24 @@ public class Main {
     double e = 3;
     double f = 4;
     double g = 5;
+    public static void wyswietl () {
+        String imie, nazwisko;
+        int wiek;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wypisz imie");
+        imie = scanner.next();
+        System.out.println("Wypisz nazwisko");
+        nazwisko = scanner.next();
+        System.out.println("Wypisz wiek");
+        wiek = scanner.nextInt();
+
+
+    }
+
 
 
     public static void main(String[] args) {
-        System.out.println("napis");
-
+        wyswietl();
 
 
 
